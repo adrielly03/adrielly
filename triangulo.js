0ef3439f2f56cfe9dfe2dff037832a2cfbb9de2f
adrielly03 <137908983+adrielly03@users.noreply.github.com>
2023-10-24 12:34:45 -0300

    Add files via upload

---
+++ triangulo.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("#outResp")
+const resp1 = document.querySelector("#outresp1")
+
+//ouvinte
+form.addEventListenner("submit", (e)=>{
+e.preventDefault()
+  
+   const l  aqadoA = number(form.ladoA.value)
+   const ladoB = number(form.ladoB.value)
+   const ladoC = number(form.ladoC.value)
+})
+
